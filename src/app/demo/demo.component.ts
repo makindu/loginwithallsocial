0f807133d66edaa35a4f6d0e316919d0dfc6f6a5
MAKINDU <72305144+makindu@users.noreply.github.com>
2021-10-09 09:41:04 +0200

    Login with Google,Facebook,Microsoft,Amazon,VK

---
+++ src/app/demo/demo.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+
+import { SocialAuthService } from 'angularx-social-login';
+import { SocialUser } from 'angularx-social-login';
+import {
+  GoogleLoginProvider,
+  FacebookLoginProvider,
+  AmazonLoginProvider,
+  VKLoginProvider,
+  MicrosoftLoginProvider
+} from 'angularx-social-login';
+
+@Component({
+  selector: 'lib-app-demo',
+  templateUrl: './demo.component.html',
+  styleUrls: ['./demo.component.css']
+})
+export class DemoComponent implements OnInit {
+
+  user: SocialUser;
+  GoogleLoginProvider = GoogleLoginProvider;
+
+  constructor(private authService: SocialAuthService) { }
+
+  ngOnInit() {
+    this.authService.authState.subscribe(user => {
+      this.user = user;
+    });
+  }
+
+  signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+  signInWithFB(): void {
+    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
+  }
+
+  signInWithAmazon(): void {
+    this.authService.signIn(AmazonLoginProvider.PROVIDER_ID);
+  }
+
+  signInWithVK(): void {
+    this.authService.signIn(VKLoginProvider.PROVIDER_ID);
+  }
+
+  signInWithMicrosoft(): void {
+    this.authService.signIn(MicrosoftLoginProvider.PROVIDER_ID);
+  }
+
+  signOut(): void {
+    this.authService.signOut();
+  }
+
+  refreshGoogleToken(): void {
+    this.authService.refreshAuthToken(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+}
